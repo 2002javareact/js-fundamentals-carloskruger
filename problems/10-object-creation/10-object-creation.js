6bd9ce137addaa86ee23699d53c0b9d2e577d8f3
Carlos Kruger <carloskr@yahoo.com>
2020-02-13 09:02:58 -0500

    thursday

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,16 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+const firstObject = {name: "Carlos", age: 100}
+
+function secondObject(name, age){
+    this.name = name;
+    this.age - age;
+}
+
+class thirdObject{
+    constructor(){
+        this.name = name;
+        this.age = age;
+    }
+}
