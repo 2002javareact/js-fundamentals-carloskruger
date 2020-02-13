116e12f83c0bc9cbedbaffe215fb1c48dad29b6e
Carlos Kruger <carloskr@yahoo.com>
2020-02-13 09:48:15 -0500

    problem 11

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,6 +2,24 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
+
+//return the same array without the element located at that index 
+
 function spliceElement(someArr, index) {
-  
+    let newArr = []
+    for(let i = 0;i < someArr.length; i++){
+        if(i!=index)
+            newArr.push(someArr[i])
+    }
+    // Emptying the array!!!
+    someArr.length = 0
+    
+    for (let j = 0; j < newArr.length; j++){
+        someArr.push(newArr[j])
+    }
+    return someArr
 }
+
+myArray = [3,4,5,6,7]
+console.log(spliceElement(myArray,3))
+
